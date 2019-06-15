b6b19e1042bdf48da3cb94f9a9df5b14a8da396c
samsamson33 <35579293+samsamson33@users.noreply.github.com>
2019-06-14 22:15:28 -0400

    Add JSONBlob data storage mock-up

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const { AkairoClient } = require("discord-akairo");
+const JSONBlob = require("jsonblob");
 
 class ColorWarsBotClient extends AkairoClient {
 	constructor() {
 		super({
 			ownerID: "317468487838728196"
 		});
+
+		this.JSONBlob = new JSONBlob(process.env.ColorWarsJSONBlobID);
+	}
+
+	async login() {
+		this.data = await this.JSONBlob.getBlob();
+	}
+
+	async fetchData() {
+		this.data = await this.JSONBlob.getBlob();
+		return this.data;
+	}
+
+	async storeData() {
+		const data = await this.JSONBlob.updateBlob(this.data);
+		return data;
 	}
 }
 
